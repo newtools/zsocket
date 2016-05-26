8feea86b31680ad94ed504ec2e4517e471150cd3
Nathan Sweet <nathanjsweet@gmail.com>
2016-05-26 16:23:24 -0500

    adding route logic

--- util/vethforcontainer.sh
+++ util/vethforcontainer.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 container=$1
 ipaddr=$2
+vethname=$3
+subnet=$4
+
 mtu=2500
 NSDIR=/var/run/netns
 containerif=eth1
@@ -19,8 +22,8 @@ if [ ! -e $NSDIR/$container ]; then
     ln -s /proc/$nspid/ns/net $NSDIR/$container
 fi
 
-localif=veth.${container}
-tempif=vethc.${container}
+localif=veth.${vethname}
+tempif=vethc.${vethname}
 ip link add name $localif type veth peer name $tempif
 
 # add local interface to bridge
@@ -30,3 +33,4 @@ ip link set $localif up
 ip link set $tempif netns $container
 ip netns exec $container ip link set $tempif up mtu $mtu name $containerif
 ip netns exec $container ip addr replace $ipaddr dev $containerif
+ip netns exec $container ip route add $subnet dev $containerif
